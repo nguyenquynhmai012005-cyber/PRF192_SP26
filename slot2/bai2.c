bec2cb66564f2baca9cd8597e8c0663f7db9fb73
nguyenquynhmai012005-cyber <nguyenquynhmai012005@gmail.com>
2026-01-09 11:35:50 +0700

    bai2

---
+++ slot2/bai2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    // Khai báo và khởi tạo biến số thực a, b
+    float a = 5.0;
+    float b = 10.216;
+
+    // Khai báo biến kq để lưu kết quả cộng
+    float kq = a + b;
+
+    // In giá trị của a và b
+    printf("a = %.0f\n", a);
+    printf("b = %.3f\n", b);
+
+    // In kết quả phép cộng
+    printf("%.0f + %.3f = %.2f\n", a, b, kq);
+
+    return 0;
+}
+

b717d7c7b463eda211d73060b0718ac117689b74
nguyenquynhmai012005-cyber <nguyenquynhmai012005@gmail.com>
2026-01-09 11:22:49 +0700

    main

---
+++ slot2/bai 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	/*khai bao 1 bien so nguyen,
+	khởi tạo giá trị = 5
+	*/ 
+	//In ra  man hinh
+	printf("a=%2d \n", a);
+	//Gán giá trị mới cho
+	a = 10;
+	printf("a=%d", a);
+	
+	return 0;
+}

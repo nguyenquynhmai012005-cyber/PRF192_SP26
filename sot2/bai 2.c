4b3e12cfed2fd54063849d6a687993de78e62148
nguyenquynhmai012005-cyber <nguyenquynhmai012005@gmail.com>
2026-01-09 11:07:21 +0700

    bai2

---
+++ sot2/bai 2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	// khai bao 1 so nguyen
+	int S; // giai thich: khai bao bien S la gia tri so nguyen
+	
+	// khai bao gia tri bat ky
+	S = 10; //giai thich: gan gia tri ho bien S la 10
+	
+	//in ra hinh
+	printf ("gia tri la: %d\n", S); // giai thich in ra man hinh bang Lenh printf , va %d la chuoi hinh dang de gia tri dat vao dung vi tri do
+	
+	return 0;
+}

b8c1c8200526f1589ecdaf9b898698d8c9696ef4
nguyenquynhmai012005-cyber <nguyenquynhmai012005@gmail.com>
2026-02-06 10:27:21 +0700

    ex1.c

---
+++ workshop/workshop/workshops1/bai1.c
@@ -0,0 +1,17 @@
+//Nhập vào biến x nguyên
+//Tính |x| 
+//Tinh sqrt(x)
+#include <stdio.h>
+#include <math.h>
+int main() {
+	int x;
+	printf("nhap x:");
+	scanf("%d",&x);
+	int preX = x;
+	if (x < 0) {
+		x = -x;
+		printf ("|%d| = %d",preX,x);
+		printf("\nCan bac 2 = %.2f",sqrt(x));
+		return 0;
+	}
+}
